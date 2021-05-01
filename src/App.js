aefca3c1f253b1c5e5858b8eaa54257329de4f7e
pr47eek <prateekpatel0512@gmail.com>
2021-05-01 17:24:17 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import CartItem from "./CartItem";
+import Cart from "./cart";
 function App() {
   return (
     <div className="App">
-    <CartItem/>
+    <Cart/>
     </div>
   );
 }
